8e8f0e0d2143bff42f19b01e4f2f3706b1a0417e
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2025-06-05 16:57:49 +0530

    Read captcha configurations from captcha config service

--- components/org.wso2.carbon.identity.application.authenticator.basicauth/src/test/java/org/wso2/carbon/identity/application/authenticator/basicauth/internal/BasicAuthenticatorServiceComponentTestCase.java
+++ components/org.wso2.carbon.identity.application.authenticator.basicauth/src/test/java/org/wso2/carbon/identity/application/authenticator/basicauth/internal/BasicAuthenticatorServiceComponentTestCase.java
@@ -17,14 +17,22 @@
  */
 package org.wso2.carbon.identity.application.authenticator.basicauth.internal;
 
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.osgi.service.component.ComponentContext;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.wso2.carbon.identity.captcha.exception.CaptchaServerException;
+import org.wso2.carbon.identity.captcha.provider_mgt.service.CaptchaConfigService;
+import org.wso2.carbon.identity.captcha.provider_mgt.service.impl.CaptchaConfigServiceImpl;
 import org.wso2.carbon.identity.configuration.mgt.core.ConfigurationManager;
 import org.wso2.carbon.user.core.service.RealmService;
 
+import java.util.Properties;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 
@@ -65,10 +73,32 @@ public class BasicAuthenticatorServiceComponentTestCase {
 
     @Test
     public void activateTestCase() {
-        ComponentContext componentContext = mock(ComponentContext.class, Mockito.RETURNS_DEEP_STUBS);
-        basicAuthenticatorServiceComponent.activate(componentContext);
 
-        Mockito.verify(componentContext, Mockito.atLeastOnce()).getBundleContext();
+        ComponentContext componentContext = mock(ComponentContext.class, Mockito.RETURNS_DEEP_STUBS);
+        Properties captchaConfig = new Properties();
+        captchaConfig.setProperty("captchaEnabled", "true");
+        captchaConfig.setProperty("captchaProvider", "testCaptcha");
+        captchaConfig.setProperty("reCaptchaSiteKey", "testSiteKey");
+        captchaConfig.setProperty("reCaptchaSiteSecret", "testSiteSecret");
+
+        try (MockedStatic<BasicAuthenticatorDataHolder> mockedStatic =
+                     mockStatic(BasicAuthenticatorDataHolder.class)) {
+
+            BasicAuthenticatorDataHolder mockDataHolder = mock(BasicAuthenticatorDataHolder.class);
+
+            // Stub methods on the mock
+            CaptchaConfigService mockCaptchaConfigService = mock(CaptchaConfigService.class);
+            when(mockCaptchaConfigService.getActiveCaptchaProviderConfig()).thenReturn(
+                    captchaConfig); // or a real config if needed
+
+            when(mockDataHolder.getCaptchaConfigService()).thenReturn(mockCaptchaConfigService);
+
+            // Return mockDataHolder when getInstance() is called
+            mockedStatic.when(BasicAuthenticatorDataHolder::getInstance).thenReturn(mockDataHolder);
+            basicAuthenticatorServiceComponent.activate(componentContext);
+
+            Mockito.verify(componentContext, Mockito.atLeastOnce()).getBundleContext();
+        }
     }
 
     @Test
